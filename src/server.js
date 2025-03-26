eaa7e24cdf07e64d84577b2b88223ae13f08e746
Rihawy <zmr@brreg.no>
2025-03-26 14:13:37 +0100

    Initial backend setup

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Parse JSON requests
+app.use(cors()); // Enable CORS
+app.use(morgan('dev')); // Log requests
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('TaxiWay API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`My server running on port ${PORT}`));
