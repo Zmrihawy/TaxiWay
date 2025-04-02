b4bfcf4a37a458f5f365e56c36b08b2a55dd5404
Rihawy <zmr@brreg.no>
2025-04-02 15:12:53 +0200

    Created User Model

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['rider', 'driver', 'admin'], default: 'rider' },
+    rating: { type: Number, default: 5.0 },
+    profileImage: { type: String }
+}, { timestamps: true });
+
+// Hash password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
