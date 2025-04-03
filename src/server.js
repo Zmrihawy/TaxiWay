4b549c28be14323e967158094775f592435f7051
Rihawy <zmr@brreg.no>
2025-04-03 20:38:19 +0200

    Updated server.js with new routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const crypto = require("crypto");
 
 // Load environment variables
 dotenv.config();
@@ -16,7 +17,10 @@ const app = express();
 // Middleware
 app.use(express.json()); // Parse JSON requests
 app.use(cors()); // Enable CORS
-app.use(morgan('dev')); // Log requests
+app.use(morgan('dev')); // Log requests,
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
 
 // Default route
 app.get('/', (req, res) => {
@@ -25,4 +29,7 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`My server running on port ${PORT}`));
+app.listen(PORT, () => {
+    const secret = crypto.randomBytes(64).toString("hex");
+    console.log("Generated JWT_SECRET:", secret);
+    console.log(`My server running on port ${PORT}`)});
