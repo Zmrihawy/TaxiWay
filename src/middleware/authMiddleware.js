9824ee51ceb4565276f71ba674e8e5b9940b59cb
Rihawy <zmr@brreg.no>
2025-04-07 12:48:07 +0200

    Updated protecting routes by allowing reole-based access control

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const protect = (req, res, next) => {
+const protect = async (req, res, next) => {
     const token = req.header('Authorization');
 
     if (!token || !token.startsWith('Bearer ')) {
@@ -9,11 +10,19 @@ const protect = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-        req.user = decoded; // Attach user info to request
+        req.user = await User.findById(decoded.id).select('-password'); // Attach user info to request
         next();
     } catch (error) {
         res.status(401).json({ message: 'Invalid token' });
     }
 };
 
-module.exports = { protect };
+const adminOnly = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403).json({ message: 'Access denied: Only Admins have access' });
+    }
+};
+
+module.exports = { protect, adminOnly };
