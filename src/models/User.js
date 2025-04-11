c68d329b2f9cd17421433fec20563f5c02ec8721
Rihawy <zmr@brreg.no>
2025-04-11 21:31:32 +0200

    Update the User Schema for Location

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,18 @@ const UserSchema = new mongoose.Schema({
     password: { type: String, required: true },
     role: { type: String, enum: ['rider', 'driver', 'admin'], default: 'rider' },
     rating: { type: Number, default: 5.0 },
-    profileImage: { type: String }
+    profileImage: { type: String },
+    currentLocation: {
+        type: {
+          type: String,
+          enum: ['Point'],
+          default: 'Point'
+        },
+        coordinates: {
+          type: [Number], // [longitude, latitude]
+          default: [0, 0]
+        }
+    }
 }, { timestamps: true });
 
 // Hash password before saving
@@ -16,6 +27,9 @@ UserSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
+
+// Create 2dsphere index for geospatial queries
+UserSchema.index({ currentLocation: '2dsphere' });
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
